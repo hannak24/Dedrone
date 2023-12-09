3058c19902f497c16eef42bbd49417ade42af18f
hannak24 <73960535+hannak24@users.noreply.github.com>
2023-12-09 23:30:54 +0200

    clean code

--- app/src/main/java/com/example/dedrone/LoginActivity.kt
+++ app/src/main/java/com/example/dedrone/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.example.dedrone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -15,18 +14,14 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setContentView(R.layout.activity_login)
         title="Login"
         auth= FirebaseAuth.getInstance()
     }
 
     fun login(view: View){
-
-        println("email")
         val emailEditText: EditText = findViewById(R.id.editTextEmailAddress)
         val email = emailEditText.text.toString()
-        Log.d("my email", email);
         val passwordEditText: EditText = findViewById(R.id.editTextPassword)
         val password = passwordEditText.text.toString()
 
--- app/src/main/java/com/example/dedrone/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dedrone.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
